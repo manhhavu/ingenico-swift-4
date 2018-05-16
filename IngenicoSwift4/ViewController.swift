0b3d271799f09cd0e4270ee59d25a19cdc42f217
manhhavu <vumanhha87@gmail.com>
2018-05-16 09:49:29 +0200

    Initial Commit

---
+++ IngenicoSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IngenicoSwift4
+//
+//  Created by Manh-Ha VU on 16/05/2018.
+//  Copyright © 2018 Manh-Ha Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
